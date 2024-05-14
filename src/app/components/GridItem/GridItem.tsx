56acf1b76e47366359350b81a214fd3bcf0b2209
SSkkky <worte5633@gmail.com>
2024-05-14 23:38:30 +0900

    필터 컴포넌트 수정

--- src/app/components/GridItem/GridItem.tsx
+++ src/app/components/GridItem/GridItem.tsx
@@ -9,19 +9,30 @@ import MinchomapComp from '@/app/components/GridItem/components/Minchomap';
 import NikeComp from '@/app/components/GridItem/components/nike';
 
 import { gridFilterStore } from '@/store/gridFilterStore'
+import { useEffect, useState } from 'react';
 
 export default function GridItem(){
     const {filter} = gridFilterStore();
+    const [comp, setComp] = useState(<><IntroComp /><AboutComp/><NikeComp/><HappypubComp/><MovieComp/><ChodingComp /><MinchomapComp/></>);
+
+    useEffect(()=>{
+    switch (filter) {
+        case 'ALL':
+            setComp(<><IntroComp /><AboutComp/><NikeComp/><HappypubComp/><MovieComp/><ChodingComp /><MinchomapComp/></>)
+            break;
+
+        case 'PERSONAL':
+            setComp(<><AboutComp/><NikeComp/><MovieComp/><MinchomapComp/></>)
+            break;
+        
+        case 'TEAM' : setComp(<><HappypubComp/><ChodingComp/></>)
+            break;
+    }
+    },[filter])
 
     return(
         <section className={`gridSection ` + filter}>
-            <IntroComp />
-            <AboutComp/>
-            <NikeComp/>
-            <HappypubComp/>
-            <MovieComp/>
-            <ChodingComp />
-            <MinchomapComp/>
+            {comp}
         </section>
     )
 }
