eec1450236a1f4fe02ea787451b34ee1846a58df
SSkkky <worte5633@gmail.com>
2024-05-17 17:09:57 +0900

    타이틀 추가

--- src/app/components/GridItem/components/Choding.tsx
+++ src/app/components/GridItem/components/Choding.tsx
@@ -7,7 +7,11 @@ import '../styles/Choding.scss';
 export default function Choding(){
     return(
         <article className="choding" id="choding">
-            <TagComp keyword={['TEAM', 'NEXT.JS']}/>
+            <section className='title'>
+                <b>초딩</b>
+                <span>초보들의 코딩공부</span>
+            </section>
+            <TagComp keyword={['TEAM', 'NEXT.JS']} />
             <Image src={IphoneImg} alt="아이폰" width={256} height={527} className='iphone'/>
         </article>
     )
