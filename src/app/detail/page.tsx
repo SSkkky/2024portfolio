b30d31b81a2a79927ff10f57c16d53eb1ea28e61
SSkkky <worte5633@gmail.com>
2024-05-27 00:43:52 +0900

    디테일 페이지 수정

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,21 +1,9 @@
-"use client"
 import data from '@/data/data.json';
-import { ListType } from '@/types/dataTypes';
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
 import { MainComp } from './components/Main';
 
-export default function Detail() {
-    const [detailData, setDetailData] = useState<ListType | undefined>();
-    const searchParams = useSearchParams();
-    const id = Number(searchParams.get('id'));
+export default async function DetailPage({ searchParams }: { searchParams: { id: string } }) {
+  const { id } = searchParams;
+  const detailData = data.find((item) => item.id === Number(id));
 
-    useEffect(()=>{
-        const filteredData = data[id];
-        setDetailData(filteredData)
-    },[id])
-
-    return(
-        <MainComp detailData={detailData} />
-    )
+  return <MainComp detailData={detailData} />;
 }
