05abfc50e667bbe3f0b1d0d1ea487a2ba433fe22
SSkkky <worte5633@gmail.com>
2024-05-26 19:29:51 +0900

    ,

--- src/app/detail/components/Main.tsx
+++ src/app/detail/components/Main.tsx
@@ -3,7 +3,7 @@ import { ListType } from '@/types/dataTypes';
 type MainType = {
     detailData : ListType | undefined
 }
-export default function Main ({detailData} : MainType){
+export function Main ({detailData} : MainType){
     return (
         <main>
             {detailData && <p>{detailData.name}의 디테일 페이지입니당</p>}
--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -3,9 +3,9 @@ import data from '@/data/data.json';
 import { ListType } from '@/types/dataTypes';
 import { useSearchParams } from 'next/navigation';
 import { Suspense, useEffect, useState } from 'react';
-import MainComp from './components/Main';
+import { Main } from './components/Main';
 
-export default function Detail (){
+export function Detail (){
     const [detailData, setDetailData] = useState<ListType>();
     const searchParams = useSearchParams();
     const id = Number(searchParams.get('id'));
@@ -17,7 +17,7 @@ export default function Detail (){
 
     return(
         <Suspense fallback={<p>로딩중...</p>}>
-            <MainComp detailData={detailData}/>
+            <Main detailData={detailData}/>
         </Suspense>
     )
 }
