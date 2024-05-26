4f9ca15a154f2eac65880738297302af35433cc4
SSkkky <worte5633@gmail.com>
2024-05-26 18:03:09 +0900

    gridfilter 수정

--- src/app/components/MainItem/Intro.tsx
+++ src/app/components/MainItem/Intro.tsx
@@ -1,4 +1,3 @@
-import Header from "./components/Header"
 import Text from "./components/Text"
 import Skill from "./components/Skill"
 import Cloud from "./components/Cloud"
@@ -7,7 +6,6 @@ import "./styles/Intro.scss"
 export default function MainItem() {
   return (
       <article id="MainItem" className="Self">
-          {/* <Header/> */}
           <Text/>
           <Skill/>
           <Cloud/>
