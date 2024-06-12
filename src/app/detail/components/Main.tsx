551b134e6e84ddfe10d0467601e1a948c9d0368a
SSkkky <worte5633@gmail.com>
2024-06-12 19:30:00 +0900

    디테일 메인 데이터 추가

--- src/app/detail/components/Main.tsx
+++ src/app/detail/components/Main.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { ListType } from '@/types/dataTypes';
 import '../styles/Main.scss';
 
@@ -5,9 +7,23 @@ type MainType = {
     detailData : ListType | undefined
 }
 export function MainComp ({detailData} : MainType){
+    console.log('----------------detailData')
+    console.log(detailData)
+
+    const onClickDetailBtn = (type : string) => {
+        type === 'github'
+        ? console.log('깃헙버튼클릭')
+        : console.log('배포버튼클릭');
+    }
+
     return (
         <main id='DetailMain'>
-            {detailData && <p>{detailData.name}의 디테일 페이지입니당</p>}
+            <article className='overview'>
+                <b>{detailData?.name}</b>
+                <p>{detailData?.dateteam}</p>
+                <button onClick={()=>{onClickDetailBtn('github')}}>깃헙</button>
+                <button onClick={()=>{onClickDetailBtn('deploy')}}>배포</button>
+            </article>
         </main>
     )
 }
